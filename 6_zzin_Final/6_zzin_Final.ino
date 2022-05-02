303049e9822dbd3a1f3dbf865f57555b9ef0e4d7
kelvin926 <kelvin@kakao.com>
2022-05-02 18:50:37 +0900

    first commit

--- 6_zzin_Final/6_zzin_Final.ino
+++ 6_zzin_Final/6_zzin_Final.ino
@@ -5,8 +5,8 @@
 #define NPIXELS 128 // 한 배열당 몇 픽셀을 넣을 것인지 정의 
 byte Pixel[NPIXELS]; // 얼마나 측정할 것인지. <0-255> (블랙 0, 흰색 255)
 int LineSensor_Data_Adaption[NPIXELS]; // 보정된 라인센서 데이터 배열
-int MAX_LineSensor_Data[NPIXELS]; // 센서의 최대값 정의
-int MIN_LineSensor_Data[NPIXELS]; // 센서의 최소값 정의
+int MAX_LineSensor_Data; // 센서의 최대값 정의
+int MIN_LineSensor_Data; // 센서의 최소값 정의
 int flag_line_adapation; // 라인센서 확인을 위한 플래그 변수
 #define FASTADC 1
 #define cbi(sfr, bit) (_SFR_BYTE(sfr) &= ~_BV(bit))
@@ -20,11 +20,11 @@ void line_adaptation(void) // 라인 보정부
     int i;
     for (i = 0; i < NPIXELS; i++)
     {
-        if (LineSensor_Data_Adaption[i] >= MAX_LineSensor_Data[i])  
-            MAX_LineSensor_Data[i] = LineSensor_Data_Adaption[i]; 
+        if (LineSensor_Data_Adaption[i] >= MAX_LineSensor_Data)  
+            MAX_LineSensor_Data = LineSensor_Data_Adaption[i]; 
         //센서 데이터가 이전의 최대 데이터보다 클 경우, 최대 데이터를 센서 데이터로 입력
-        if (LineSensor_Data_Adaption[i] <= MIN_LineSensor_Data[i])
-            MIN_LineSensor_Data[i] = LineSensor_Data_Adaption[i]; 
+        if (LineSensor_Data_Adaption[i] <= MIN_LineSensor_Data)
+            MIN_LineSensor_Data = LineSensor_Data_Adaption[i]; 
         //센서 데이터가 이전의 최소 데이터보다 작을 경우, 최소 데이터를 센서 데이터로 입력
     }
 }
@@ -40,17 +40,17 @@ void read_line_sensor(void) //라인 센싱부
     digitalWrite (SIpin, LOW); //SI핀 대기상태 - 카메라 송수신 중단.
     delayMicroseconds (1); //1ms 대기
 
-    for (i = 0; i < NPIXELS; i++)
+    for (i = 0; i < NPIXELS; i++) // 원본 배열에 넣는 for문
     {
-        Pixel[i] = analogRead (AOpin) / 4 ; // 8-bit is enough - 픽셀배열에 각 이미지 픽셀 값을 넣음
+        Pixel[i] = analogRead (AOpin) / 4 ; // 8-bit is enough - 픽셀배열에 각 이미지 픽셀 값을 넣음 // 원본 데이터 입니다.
         digitalWrite (CLKpin, LOW); // CLK핀 대기상태 - 데이터 동기화 중지 (위 코드에서 High인 상태에서 해당 반복문이 시작되기에, 첫 픽셀은 기본으로 들어감.)
         delayMicroseconds (1); // 1ms대기
         digitalWrite (CLKpin, HIGH); //CLK핀 작동상태 - 데이터 동기화 시작 (이게, 데이터가 많으니까 한 픽셀에 들어갈 양만 clk값에 따라 딱 넣고 멈췄다가 다음 픽셀에 넣고 멈췄다가 이런 느낌인가봐.)
     }
     
-    for (i = 0; i < NPIXELS; i++)
+    for (i = 0; i < NPIXELS; i++) // 0 ~ 1023까지의 데이터를 2번 과정을 거친 뒤에 -> 0 ~ 256
     {
-        LineSensor_Data_Adaption[i] = map(Pixel[i], MIN_LineSensor_Data[i], MAX_LineSensor_Data[i], 0, 256);
+        LineSensor_Data_Adaption[i] = map(Pixel[i], MIN_LineSensor_Data, MAX_LineSensor_Data, 0, 256);
     }
 }
 // ------------------ 라인 센싱 초기 설정 끝 --------------------------------
@@ -76,7 +76,7 @@ void motor_control(int direction, int speed) //dc모터 컨트롤 함수 생성
 
 // -------------------------------- 서보모터 초기 설정 시작 --------------------------------
 #include <Servo.h> //Servo라이브러리 아두이노 프로그램에 설치해야합니당 아마 기본으로 깔려있을껄..?
-#define RC_SERVO_PIN 8 //8번핀 할당
+#define RC_SERVO_PIN 12 //12번핀 할당
 #define NEURAL_ANGLE 90 //기본 앵글: 115도 -> 전방 방향
 #define LEFT_STEER_ANGLE -30 //좌측 스티어링 각도 지정
 #define RIGHT_STEER_ANGLE 30 //우측 스티어링 각도 지정
@@ -142,20 +142,24 @@ void Two_Line(void)
 // -------------------------------- 초음파 시스템 시작 --------------------------------
 #define DEBUG 1 // 디버깅 코드는 왜있을까~~~~
 #include <NewPing.h> //초음파 관련 헤더파일임! (설치 필요)
-#define SONAR_NUM 1 //초음파 센서 번호를 부여 (1번)
+#define SONAR_NUM 3 //초음파 센서 갯수를 부여 (3번)
 #define MAX_DISTANCE 150 //cm단위 최대 값 (초음파 최대값)
 float UltrasonicSensorData[SONAR_NUM];//센서 데이터 1차배열 생성(실수)
 
 NewPing sonar[SONAR_NUM] = {   // 초음파센서 배열 생성 
-  NewPing(9, 10, MAX_DISTANCE)  // NewPing(Trig(송신)핀 번호, Echo(수신)핀 번호, 최대측정거리) [초음파] (개체 설정)
+    NewPing(8, 9, MAX_DISTANCE)  // NewPing(Trig(송신)핀 번호, Echo(수신)핀 번호, 최대측정거리) [초음파] (개체 설정) 앞
+    NewPing(2, 3, MAX_DISTANCE)  // NewPing(Trig(송신)핀 번호, Echo(수신)핀 번호, 최대측정거리) [초음파] (개체 설정) 왼쪽
+    NewPing(10, 11, MAX_DISTANCE)  // NewPing(Trig(송신)핀 번호, Echo(수신)핀 번호, 최대측정거리) [초음파] (개체 설정) 오른쪽
 };
+
 void read_ultrasonic_sensor(void) //초음파 값 읽어들이는 함수 
 {
-   UltrasonicSensorData[0]= sonar[0].ping_cm(); //해당 배열에 첫번째 초음파 센서를 이용한 cm단위의 값을 넣음
-
-   if(UltrasonicSensorData[0] == 0.0) //0.0을 넣었을 경우(완전 붙었다기보단 너무 멀어져서 값이 0이 된 것.)
-    {
-        UltrasonicSensorData[0] = (float)MAX_DISTANCE; //측정값이 매우 커서 0이 나왔을 경우, 넣을 수 있는 최대값을 넣는다.
+    for(int i=0; i<=2; i++){
+        UltrasonicSensorData[i]= sonar[i].ping_cm(); //해당 배열에 첫번째 초음파 센서를 이용한 cm단위의 값을 넣음
+        if(UltrasonicSensorData[i] == 0.0) //0.0을 넣었을 경우(완전 붙었다기보단 너무 멀어져서 값이 0이 된 것.)
+        {
+            UltrasonicSensorData[i] = (float)MAX_DISTANCE; //측정값이 매우 커서 0이 나왔을 경우, 넣을 수 있는 최대값을 넣는다.
+        }
     }
 }
 // -------------------------------- 초음파 시스템 끝 --------------------------------
@@ -169,8 +173,8 @@ void setup() {
     for (i = 0; i < NPIXELS; i++) // NPIXELS = 128임.
     {
         LineSensor_Data_Adaption[i] = 0; //이미지를 나타내는 전체 픽셀들 보정 데이터를 0으로 초기화
-        MAX_LineSensor_Data[i] = 1023; //최대값을 전부 1023으로 초기화
-        MIN_LineSensor_Data[i] = 0; //최소값을 전부 0으로 초기화
+        MAX_LineSensor_Data = 1023; //최대값을 1023으로 초기화
+        MIN_LineSensor_Data = 0; //최소값을 0으로 초기화
     }
     pinMode(SIpin, OUTPUT); //카메라 SI(카메라 시작) 디지털 핀 출력모드
     pinMode(CLKpin, OUTPUT); //카메라 CLK(카메라 데이터 전송 주기 동기화) 디지털 핀 출력모드
@@ -193,6 +197,12 @@ void setup() {
     pinMode(MOTOR_PWM, OUTPUT); //DC모터 속도 핀을 디지털 출력 핀으로 지정
 // -------------------------------- DC모터 셋업 부분 끝 --------------------------------
     Serial.begin(115200); // 115200속도로 시리얼 전송~
+    while(1){ // 스타트 함수
+        read_ultrasonic_sensor();
+        if UltrasonicSensorData[0] >= 100 {
+            break;
+        }
+    }
 }
 // -------------------------------- 셋업 END --------------------------------
 
